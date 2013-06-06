259157b5d72fdc5e82fd0539c4298a040cf1baca
John Maddux <jemaddux@gmail.com>
2013-06-06 13:31:39 -0600

    handling non-existent pages

--- wiki.go
+++ wiki.go
@@ -43,13 +43,17 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
   title := r.URL.Path[lenPath:]
-  p, _ := loadPage(title)
+  p, err := loadPage(title)
+  if err != nil {
+    http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+    return
+  }
   renderTemplate(w, "view", p)
 }
 
 func main() {
   http.HandleFunc("/view/", viewHandler)
   http.HandleFunc("/edit/", editHandler)
-  http.HandleFunc("/save/", saveHandler)
+  // http.HandleFunc("/save/", saveHandler)
   http.ListenAndServe(":8080", nil)
 }
